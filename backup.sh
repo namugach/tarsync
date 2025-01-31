438282331b05d6ca0f213fbd36dffaa025e38e76
namugach <namugach@gmail.com>
2025-02-01 06:09:40 +0900

    수정: 백업 스크립트 경로 수정 및 압축 옵션 변경
    - 백업 파일 저장 경로를 절대 경로에서 상대 경로로 변경
    - 압축 옵션 변경 (-cpjf -> -cf -P) 및 `tee` 명령어를 사용하여 압축과 동시에 파일 저장
    - `cdrom`, `swapfile` 디렉터리 백업 제외 목록에 추가

--- backup.sh
+++ backup.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
-mkdir -p ./source
-BACKUP_DIR=$(pwd)/source
+BASE_PATH=$(pwd)
+BACKUP_DIR=$BASE_PATH/source
+mkdir -p $BACKUP_DIR
 DATE=$(date +%Y_%m_%d_%p_%I_%M_%S)  # 예: 2025_02_01_AM_01_23_30
-TAR_FILE="${BACKUP_DIR}/root_backup_${DATE}.tar.gz"
+TAR_FILE="${BACKUP_DIR}/root_backup_${DATE}.tar"
+
 
 
 # 제외 디렉토리 설정
 EXCLUDE_DIRS="--exclude=/proc \
 --exclude=/swap.img \
 --exclude=/sys \
+--exclude=/cdrom \
 --exclude=/dev \
 --exclude=/run \
 --exclude=/tmp \
@@ -17,11 +20,10 @@ EXCLUDE_DIRS="--exclude=/proc \
 --exclude=/var/run \
 --exclude=/var/tmp \
 --exclude=/lost+found \
---exclude=/swapfile \
 --exclude=/var/lib/docker \
 --exclude=/var/lib/containerd \
---exclude=/var/run/docker.sock"
-
+--exclude=/var/run/docker.sock \
+--exclude=/swapfile"
 
 
 
@@ -32,8 +34,10 @@ echo "백업할 데이터 크기: ${TOTAL_SIZE_GB} GB"
 
 echo "백업을 시작합니다: ${TAR_FILE}"
 cd /
-sudo tar -cpjf - $EXCLUDE_DIRS \
---one-file-system -C / . | \
-pv -s $TOTAL_SIZE > $TAR_FILE
+sudo tar -cf - \
+-P \
+$EXCLUDE_DIRS \
+--one-file-system / | pv -s $TOTAL_SIZE | sudo tee $TAR_FILE > /dev/null
+
 
 echo "백업 완료: ${TAR_FILE}"
