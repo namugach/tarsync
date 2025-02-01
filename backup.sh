4ca6af4159723f1f3351da125ce4e12128287245
namugach <namugach@gmail.com>
2025-02-02 07:24:09 +0900

    개선: 루트 백업 스크립트에 압축 기능 추가
    루트 백업 스크립트(backup.sh)에 gzip 압축 기능을 추가하여 백업 파일의 크기를 줄이고 저장 공간을 효율적으로 사용하도록 개선했습니다. 또한, .gitignore 파일에 .vscode 디렉토리를 추가하여 VSCode 관련 설정 파일이 저장소에 포함되지 않도록 했습니다.
    
    - 백업 스크립트에 gzip 압축 기능 추가
    - 압축률 및 압축된 파일 크기 출력
    - 원본 tar 파일 삭제
    - .gitignore에 .vscode 디렉토리 추가

--- backup.sh
+++ backup.sh
@@ -5,8 +5,6 @@ mkdir -p $BACKUP_DIR
 DATE=$(date +%Y_%m_%d_%p_%I_%M_%S)  # 예: 2025_02_01_AM_01_23_30
 TAR_FILE="${BACKUP_DIR}/root_backup_${DATE}.tar"
 
-
-
 # 제외 디렉토리 설정
 EXCLUDE_DIRS="--exclude=/proc \
 --exclude=/swap.img \
@@ -25,8 +23,6 @@ EXCLUDE_DIRS="--exclude=/proc \
 --exclude=/var/run/docker.sock \
 --exclude=/swapfile"
 
-
-
 echo "백업할 데이터의 크기를 재고 있습니다."
 TOTAL_SIZE=$(sudo du -sb $EXCLUDE_DIRS / | awk '{print $1}')
 TOTAL_SIZE_GB=$(echo "scale=2; $TOTAL_SIZE/1024/1024/1024" | bc)
@@ -39,5 +35,29 @@ sudo tar -cf - \
 $EXCLUDE_DIRS \
 --one-file-system / | pv -s $TOTAL_SIZE | sudo tee $TAR_FILE > /dev/null
 
-
 echo "백업 완료: ${TAR_FILE}"
+
+# 압축을 시작합니다.
+echo "압축을 시작합니다."
+GZ_FILE="${TAR_FILE}.gz"
+# pv로 진행 상황을 출력하면서 압축하기
+pv $TAR_FILE | gzip > $GZ_FILE
+
+# 압축된 파일의 크기 구하기
+COMPRESSED_SIZE=$(du -sb $GZ_FILE | awk '{print $1}')
+COMPRESSED_SIZE_GB=$(echo "scale=2; $COMPRESSED_SIZE/1024/1024/1024" | bc)
+
+# 원본 파일 크기와 압축된 파일 크기 출력
+echo "원본 파일 크기: ${TOTAL_SIZE_GB} GB"
+echo "압축된 파일 크기: ${COMPRESSED_SIZE_GB} GB"
+
+# 압축률 계산 (백분율로 출력)
+COMPRESSION_RATIO=$(echo "scale=2; (1 - $COMPRESSED_SIZE / $TOTAL_SIZE) * 100" | bc)
+echo "압축률: ${COMPRESSION_RATIO}%"
+
+# 백업이 완료되었음을 알림
+echo "백업 및 압축 완료: ${GZ_FILE}"
+
+# 원본 파일을 제거합니다.
+echo "원본 tar 파일을 삭제합니다: ${TAR_FILE}"
+sudo rm -f $TAR_FILE
