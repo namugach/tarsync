15d150da5557543eb9bf11b7e4e181f029105d8b
namugach <namugach@gmail.com>
2025-02-02 14:16:35 +0900

    개선: 백업 스크립트 기능 향상 및 정보 출력 추가
    백업 스크립트를 개선하여 백업 과정의 명확성과 정보 제공을 향상시켰습니다.  주요 변경 사항은 다음과 같습니다:
    
    - 백업 파일명을 `root_backup`에서 `tarsync`로 변경.
    - 백업 디렉토리 경로를 절대 경로로 명시.
    - 백업 및 압축 과정에 대한 상세 정보 출력 (백업 크기, 압축률 등).
    - 압축 과정에 pv 명령어를 사용하여 진행률 표시.
    - 원본 tar 파일 삭제 후 source 디렉토리 내 최근 5개 파일 정보 출력.
    - 주석 추가 및 코드 정리로 가독성 향상.

--- backup.sh
+++ backup.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
+
+# 현재 작업 디렉토리를 기준으로 백업 디렉토리 설정
 BASE_PATH=$(pwd)
 BACKUP_DIR=$BASE_PATH/source
-mkdir -p $BACKUP_DIR
-DATE=$(date +%Y_%m_%d_%p_%I_%M_%S)  # 예: 2025_02_01_AM_01_23_30
-TAR_FILE="${BACKUP_DIR}/root_backup_${DATE}.tar"
+mkdir -p $BACKUP_DIR  # 백업 디렉토리가 없으면 생성
 
-# 제외 디렉토리 설정
+# 현재 날짜와 시간을 파일명에 포함 (예: 2025_02_01_AM_01_23_30)
+DATE=$(date +%Y_%m_%d_%p_%I_%M_%S)
+TAR_FILE="${BACKUP_DIR}/tarsync_${DATE}.tar"
+
+# 제외할 디렉토리 목록 설정
 EXCLUDE_DIRS="--exclude=/proc \
 --exclude=/swap.img \
 --exclude=/sys \
@@ -23,11 +27,13 @@ EXCLUDE_DIRS="--exclude=/proc \
 --exclude=/var/run/docker.sock \
 --exclude=/swapfile"
 
+# 백업할 데이터의 크기를 계산
 echo "백업할 데이터의 크기를 재고 있습니다."
 TOTAL_SIZE=$(sudo du -sb $EXCLUDE_DIRS / | awk '{print $1}')
 TOTAL_SIZE_GB=$(echo "scale=2; $TOTAL_SIZE/1024/1024/1024" | bc)
 echo "백업할 데이터 크기: ${TOTAL_SIZE_GB} GB"
 
+# tar 백업 파일 생성
 echo "백업을 시작합니다: ${TAR_FILE}"
 cd /
 sudo tar -cf - \
@@ -37,27 +43,34 @@ $EXCLUDE_DIRS \
 
 echo "백업 완료: ${TAR_FILE}"
 
-# 압축을 시작합니다.
+# TAR 파일 용량 출력
+TAR_FILE_SIZE=$(du -sh $TAR_FILE | awk '{print $1}')
+echo "원본 TAR 파일 크기: ${TAR_FILE_SIZE}"
+
+# 압축 과정 시작
 echo "압축을 시작합니다."
 GZ_FILE="${TAR_FILE}.gz"
-# pv로 진행 상황을 출력하면서 압축하기
+
+# tar 파일을 gzip으로 압축하며 진행률 출력
 pv $TAR_FILE | gzip > $GZ_FILE
 
-# 압축된 파일의 크기 구하기
+# 압축된 파일의 크기 계산
 COMPRESSED_SIZE=$(du -sb $GZ_FILE | awk '{print $1}')
 COMPRESSED_SIZE_GB=$(echo "scale=2; $COMPRESSED_SIZE/1024/1024/1024" | bc)
 
-# 원본 파일 크기와 압축된 파일 크기 출력
 echo "원본 파일 크기: ${TOTAL_SIZE_GB} GB"
 echo "압축된 파일 크기: ${COMPRESSED_SIZE_GB} GB"
 
-# 압축률 계산 (백분율로 출력)
+# 압축률 계산 (백분율 출력)
 COMPRESSION_RATIO=$(echo "scale=2; (1 - $COMPRESSED_SIZE / $TOTAL_SIZE) * 100" | bc)
 echo "압축률: ${COMPRESSION_RATIO}%"
 
-# 백업이 완료되었음을 알림
 echo "백업 및 압축 완료: ${GZ_FILE}"
 
-# 원본 파일을 제거합니다.
+# 원본 tar 파일 삭제
 echo "원본 tar 파일을 삭제합니다: ${TAR_FILE}"
 sudo rm -f $TAR_FILE
+
+# source 디렉토리 안 최근 5개 파일을 강조하여 출력
+echo -e "\033[1;32msource 디렉토리 내 최근 5개 파일:\033[0m"
+ls -alh $BACKUP_DIR | tail -n 5
