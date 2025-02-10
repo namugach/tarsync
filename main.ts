d83712db77560de617ba7ff93f09bfd45c9a796a
namugach <namugach@gmail.com>
2025-02-11 07:15:17 +0900

    리팩토링: 불필요한 warp 함수 제거
    `_warp` 함수를 제거하고 로직을 최상위 레벨로 이동하여 코드 구조를 단순화했습니다. 이 변경으로 인해 코드의 가독성이 향상되고 불필요한 함수 호출이 제거되었습니다.

--- main.ts
+++ main.ts
@@ -4,42 +4,38 @@ import DiskFree from "./src/util/DiskFree.ts";
 import BackupManager from "./src/compo/BackupManager.ts";
 
 
-async function _warp() {
-  const BACKUP_DISK_PATH = "/";
-  const STORE_DIR_PATH = util.getStoreDirPath();
-  const WORK_DIR_PATH = await util.getWorkDirPath();
-  const BACKUP_FILE_PATH = `${WORK_DIR_PATH}/tarsync.tar.gz`;
-  const logger = new Logger(WORK_DIR_PATH);
-  const diskfree = await util.getDiskFree(BACKUP_DISK_PATH);
-  const rootTotalUsedByte = await util.getDiskFreeWithPathByte(diskfree.mount);
-  const fanalSize = await util.calculateFinalDiskUsage(diskfree, rootTotalUsedByte);
-  const df = new DiskFree(STORE_DIR_PATH);
-  await df.load();
+const BACKUP_DISK_PATH = "/";
+const STORE_DIR_PATH = util.getStoreDirPath();
+const WORK_DIR_PATH = await util.getWorkDirPath();
+const BACKUP_FILE_PATH = `${WORK_DIR_PATH}/tarsync.tar.gz`;
+const logger = new Logger(WORK_DIR_PATH);
+const diskfree = await util.getDiskFree(BACKUP_DISK_PATH);
+const rootTotalUsedByte = await util.getDiskFreeWithPathByte(diskfree.mount);
+const fanalSize = await util.calculateFinalDiskUsage(diskfree, rootTotalUsedByte);
+const df = new DiskFree(STORE_DIR_PATH);
+await df.load();
 
-  /* 실행할 때 프로그램 유효성 검사 */
-  await util.ensureCommandExists("pv", "sudo apt install pv");
-  await util.ensureCommandExists("rsync", "sudo apt install rsync");
-  await util.ensureCommandExists("tar", "sudo apt install tar");
+/* 실행할 때 프로그램 유효성 검사 */
+await util.ensureCommandExists("pv", "sudo apt install pv");
+await util.ensureCommandExists("rsync", "sudo apt install rsync");
+await util.ensureCommandExists("tar", "sudo apt install tar");
 
-  /* 용량 확인 */
-  util.checkBackupStoreSize(df, fanalSize);
+/* 용량 확인 */
+util.checkBackupStoreSize(df, fanalSize);
 
-  /* 디렉토리 생성 */
-  await util.createStoreDir();
-  await util.mkdir(WORK_DIR_PATH);
+/* 디렉토리 생성 */
+await util.createStoreDir();
+await util.mkdir(WORK_DIR_PATH);
 
-  /* log.md 파일 작성 */
-  await logger.choiceWirte();
+/* log.md 파일 작성 */
+await logger.choiceWirte();
 
-  /* 백업 시작 메시지 */
-  console.log("📂 백업을 시작합니다.");
-  console.log(`📌 저장 경로: ${BACKUP_FILE_PATH}`);
+/* 백업 시작 메시지 */
+console.log("📂 백업을 시작합니다.");
+console.log(`📌 저장 경로: ${BACKUP_FILE_PATH}`);
 
-  /* 백업 시작 */
-  await util.backup(BACKUP_DISK_PATH, BACKUP_FILE_PATH, util.getExclude());
+/* 백업 시작 */
+await util.backup(BACKUP_DISK_PATH, BACKUP_FILE_PATH, util.getExclude());
 
-  const bm = new BackupManager(STORE_DIR_PATH);
-  console.log(await bm.printBackups(5, -1, -1));
-}
-
-await _warp();
+const bm = new BackupManager(STORE_DIR_PATH);
+console.log(await bm.printBackups(5, -1, -1));
